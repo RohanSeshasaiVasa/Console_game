8cb165263fac094dcb578c7db4b64127d730fd36
Rohan Seshasai Vasa <rohanseshasai.vasa@gmail.com>
2026-05-08 13:31:56 +0530

    Add files via upload

---
+++ Game_Console_Arduino.ino
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel matrix(16, 6, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  matrix.begin();
+  matrix.show();
+}
+
+void loop() {
+  for (int i = 0; i < matrix.numPixels(); i++) {
+    matrix.setPixelColor(i, 0, 5, 0);
+    matrix.show();
+  }
+}
