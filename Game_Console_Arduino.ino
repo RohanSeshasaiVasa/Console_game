d48428a605500d040f8d20da50aa31edb2e8e3e9
Rohan Seshasai Vasa <rohanseshasai.vasa@gmail.com>
2026-05-08 15:18:14 +0530

    Add files via upload

--- Game_Console_Arduino.ino
+++ Game_Console_Arduino.ino
@@ -5,12 +5,17 @@ Adafruit_NeoPixel matrix(16, 6, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   matrix.begin();
-  matrix.show();
 }
 
 void loop() {
   for (int i = 0; i < matrix.numPixels(); i++) {
     matrix.setPixelColor(i, 0, 5, 0);
+    matrix.show();
+    delay(200);
+  }
+
+  for (int i = 0; i < matrix.numPixels(); i++) {
+    matrix.setPixelColor(i, 0, 0, 0);
     matrix.show();
   }
 }
