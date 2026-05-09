13c63de9e35e2de2a522cf03eb59da746233d32e
Rohan Seshasai Vasa <rohanseshasai.vasa@gmail.com>
2026-05-09 13:15:26 +0530

    Added debugging logs
    Added debugging logs to know which random numbers are being generated.

--- Game_Console_Arduino.ino
+++ Game_Console_Arduino.ino
@@ -5,14 +5,20 @@ Adafruit_NeoPixel matrix(16, 6, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   matrix.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (int i = 0; i < matrix.numPixels(); i++) {
-    matrix.setPixelColor(i, 0, 5, 0);
-    matrix.show();
-    delay(200);
-  }
   matrix.clear();
-
+  for (int i = 0; i<4; i++) {
+    int pixel = random(16);
+    Serial.print("Pixel : ");
+    Serial.print(pixel);
+    Serial.print(", ");
+    
+    matrix.setPixelColor(pixel, 5, 0, 0);
+  }
+    Serial.println("");
+    matrix.show();
+    delay(5000);
 }
