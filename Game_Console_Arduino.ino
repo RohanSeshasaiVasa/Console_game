ea786fe3c09072cf94dcd7068ff6744cd939c677
Rohan Seshasai Vasa <rohanseshasai.vasa@gmail.com>
2026-05-12 13:19:13 +0530

    Made 2 types of LEDs GREEN and RED which show with delay in between.

--- Game_Console_Arduino.ino
+++ Game_Console_Arduino.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 
 Adafruit_NeoPixel matrix(16, 6, NEO_GRB + NEO_KHZ800);
-int randomNum[4];
+int randomNum[6];
 
 void setup() {
   matrix.begin();
@@ -10,10 +10,10 @@ void setup() {
 }
 
 void loop() {
-  int count = 0;
   matrix.clear();
+  int count = 0;
   
-  while (count < 4) {
+  while (count < 6) {
     int r = random(16);
     bool isDuplicate = false;
   
@@ -29,16 +29,33 @@ void loop() {
     count++;
       }
     }
-  
-    Serial.print("Pixel : ");
+    matrix.clear();
+
+    Serial.print("Pixel Red : ");
     for (int i = 0; i<4; i++) {
       Serial.print(randomNum[i]);
       Serial.print(" ");
+      matrix.setPixelColor(randomNum[i], 5, 0, 0);
+    }
+    matrix.show();
+    delay(1000);
+    matrix.clear();
+    matrix.show();
+    delay(300);
 
-      matrix.setPixelColor(randomNum[i], 100, 0, 0);
+    Serial.print("Pixel Green : ");
+    for (int i = 4; i<6; i++) {
+      Serial.print(randomNum[i]);
+      Serial.print(" ");
+      matrix.setPixelColor(randomNum[i], 0, 5, 0);
     }
     Serial.println();
     matrix.show();
-    delay(5000);
+    delay(1000);
+
+    matrix.clear();
+    matrix.show();
+
+    delay(300);
   }
 
