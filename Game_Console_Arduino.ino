f1c5f068ed5264cf026e77e0adf0be77c451c694
Rohan Seshasai Vasa <rohanseshasai.vasa@gmail.com>
2026-05-12 12:06:44 +0530

    Generate random numbers without duplication.

--- Game_Console_Arduino.ino
+++ Game_Console_Arduino.ino
@@ -1,24 +1,44 @@
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
 Adafruit_NeoPixel matrix(16, 6, NEO_GRB + NEO_KHZ800);
+int randomNum[4];
 
 void setup() {
   matrix.begin();
   Serial.begin(9600);
+  randomSeed(analogRead(0));
 }
 
 void loop() {
+  int count = 0;
   matrix.clear();
-  for (int i = 0; i<4; i++) {
-    int pixel = random(16);
+  
+  while (count < 4) {
+    int r = random(16);
+    bool isDuplicate = false;
+  
+      for (int i = 0; i < count; i++) {
+        if (randomNum[i] == r) {
+          isDuplicate = true;
+          break;
+        }
+      }
+  
+  if (!isDuplicate) {
+    randomNum[count] = r;
+    count++;
+      }
+    }
+  
     Serial.print("Pixel : ");
-    Serial.print(pixel);
-    Serial.print(", ");
-    
-    matrix.setPixelColor(pixel, 5, 0, 0);
-  }
-    Serial.println("");
+    for (int i = 0; i<4; i++) {
+      Serial.print(randomNum[i]);
+      Serial.print(" ");
+
+      matrix.setPixelColor(randomNum[i], 100, 0, 0);
+    }
+    Serial.println();
     matrix.show();
-    delay(5000);
-}
+    delay(1000);
+  }
+
