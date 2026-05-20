334b6876f798b79ef28c692c1fb69c177ff892e4
Rohan Seshasai Vasa <rohanseshasai.vasa@gmail.com>
2026-05-20 18:14:16 +0530

    Changed the layout and kept Vertical movement

--- Game_Console_Arduino.ino
+++ Game_Console_Arduino.ino
@@ -95,106 +95,80 @@ bool isClicked(int btn) {
 
 
 
-
-
-/*
-int joystickX = A2;
-int joystickY = A3;
-int position = 0;
-
-void setup() {
-  Serial.begin(9600);
-}
-
-void loop() {
-  int xValue = analogRead(joystickX);
-  // Joystick moved LEFT
-  if (xValue < 300) {
-    position = position - 1;
-    Serial.print("Horizontal : ");
-    Serial.println(position);
-    delay(300);
-  }
-
-  // Joystick moved RIGHT
-  else if (xValue > 700) {
-    position = position + 1;
-    Serial.print("Horizontal : ");
-    Serial.println(position);
-    delay(300);
-  }
-
-  int yValue = analogRead(joystickY);
-  // Joystick moved LEFT
-  if (yValue < 300) {
-    position = position - 1;
-    Serial.print("Vertical : ");
-    Serial.println(position);
-    delay(300);
-  }
-
-  // Joystick moved RIGHT
-  else if (yValue > 700) {
-    position = position + 1;
-    Serial.print("Vertical : ");
-    Serial.println(position);
-    delay(300);
-  }
-}
-*/
-
-
-
-
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #define SW 4
 Adafruit_NeoPixel matrix(16, 6, NEO_GRB + NEO_KHZ800);
 
 int joystickX = A2;
+int joystickY = A3;
 int position = 0;
+
+int convertPosition(int pos) {
+  int row = pos / 4;
+  int col = pos % 4;
+  if (row % 2 == 1) {
+    col = 3 - col;
+  }
+  return row * 4 + col;
+}
 
 void setup() {
   matrix.begin();
-  Serial.begin(9600);
-
-  pinMode(SW, INPUT_PULLUP);
-
-  randomSeed(analogRead(A0));
-
-  matrix.clear();
   matrix.show();
-  
+  Serial.begin(9600);
+  pinMode(SW, INPUT_PULLUP);
+  randomSeed(analogRead(A0));
 }
 
 void loop() {
   int xValue = analogRead(joystickX);
+  int yValue = analogRead(joystickY);
 
   if (digitalRead(SW) == LOW) {
     position = random(16);
     Serial.print("Pixel : ");
     Serial.print(position);
     matrix.clear();
-    matrix.setPixelColor(position, 5, 0, 0);
-    Serial.println("");
+    int ledNumber = convertPosition(position);
+    matrix.setPixelColor(ledNumber, 5, 0, 0);
     matrix.show();
     delay(1000);
-    matrix.clear();
   }
-  if (xValue < 300 && position < 15) {
+  // Horizontal movement
+  if (xValue < 300 && position > 0) {
     position--;
     matrix.clear();
-    matrix.setPixelColor(position, 5, 0, 0);
+    int ledNumber = convertPosition(position);
+    matrix.setPixelColor(ledNumber, 5, 0, 0);
     matrix.show();
     delay(200);
   }
 
-  if (xValue > 700 && position > 0) {
+  if (xValue > 700 && position < 15) {
     position++;
     matrix.clear();
-    matrix.setPixelColor(position, 5, 0, 0);
+    int ledNumber = convertPosition(position);
+    matrix.setPixelColor(ledNumber, 5, 0, 0);
     matrix.show();
     delay(200);
   }
-  
+  // Vertical movement
+  if (yValue < 300 && position > 0) {
+    position = position + 4;
+    matrix.clear();
+    int ledNumber = convertPosition(position);
+    matrix.setPixelColor(ledNumber, 5, 0, 0);
+    matrix.show();
+    delay(200);
+  }
+
+  if (yValue > 700 && position < 15) {
+    position = position - 4;
+    matrix.clear();
+    int ledNumber = convertPosition(position);
+    matrix.setPixelColor(ledNumber, 5, 0, 0);
+    matrix.show();
+    delay(200);
+  }
 }
